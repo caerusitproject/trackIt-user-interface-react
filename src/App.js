983ed0b28bd86b89b63128461b3cdf40122ef0d9
debanjansinha979-tech <debanjansinha979@gmail.com>
2025-09-19 17:14:39 +0530

    ..path was chnaged for App.Js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import MainApp from './Components/MainApp';
 import { Provider } from 'react-redux';
-import store from './store';
+import store from './stores/store';
 
 function App() {
   return (
