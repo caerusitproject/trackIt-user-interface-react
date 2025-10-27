232c7388d95403b5dca576e5b7d61c1003f42ac5
debanjansinha979-tech <debanjansinha979@gmail.com>
2025-10-27 18:29:11 +0530

    ...Categories and Sybcategories Added to the Menu Layer

--- src/Components/Assets/AssetMenuBuilderDialogue.js
+++ src/Components/Assets/AssetMenuBuilderDialogue.js
@@ -7,6 +7,8 @@ import {
   TextField,
   Button
 } from "@mui/material";
+import * as actions from '../../stores/actions'
+import { useDispatch } from 'react-redux';
 
 export default function AssetMenuBuilderDialogue({
   open,
@@ -14,22 +16,98 @@ export default function AssetMenuBuilderDialogue({
   additionType,
   modalData,
   setModalData,
-  sendDatatoParent
+  sendDatatoParent,
+  editItem,
+  categoryId,
+  typeholder
 }) {
+  console.log('edited item____',editItem)
+  const dispatch = useDispatch();
 
-    const handleChildData = (type) => {
-        sendDatatoParent(type)
-     }
+ React.useEffect(() => {
+    if(editItem){
+      console.log('editItem filter',editItem,typeholder)
+      if(typeholder === 'subcategory'){
+        setModalData({
+          title: editItem.name,
+          code: editItem.code,
+          description: editItem.description
+        });
+      }else{
+        setModalData({
+          title: editItem.categoryName,
+          code: editItem.categoryCode,
+          description: editItem.description
+        });
+    }
+  }else{
+    return
+  }
+  }, [editItem])
+  
+
+    const handleCreateCategoryOrSubcategory = () => {
+      if(additionType === "category-item"){
+        console.log('Creating or Editing Category Item')
+        const data ={
+          "categoryCode": modalData.code,
+          "categoryName": modalData.title,
+          "description": modalData.description
+        }
+        console.log('category data___', data)
+        dispatch(actions.createOrEditCategoryItem(data))
+      }
+      else if(additionType === "subcategory-item"){
+        console.log('Creating or Editing Subcategory Item',modalData);
+        const dataSubCategory ={
+          "categoryId": categoryId,
+          "code": modalData.code,
+          "name": modalData.title,  
+          "description": modalData.description
+        }
+        console.log('subcategory data___', dataSubCategory)
+        dispatch(actions.createOrEditSubcategoryItem(dataSubCategory))
+      }else if(additionType === "edit-category-item"){
+        const editedData ={
+          "id": editItem.id,
+          "categoryCode": modalData.code,
+          "categoryName": modalData.title,
+          "description": modalData.description
+        }
+        console.log('Editing Category Item', editedData)
+         dispatch(actions.createOrEditCategoryItem(editedData))
+      }else if(additionType === "edit-subcategory-item"){
+        const editedSubcategoryData ={
+          "categoryId": categoryId,
+          "subcategoryId": editItem.id,
+          "code": modalData.code,
+          "name": modalData.title,  
+          "description": modalData.description
+        }
+        console.log('Editing Subcategory Item', editedSubcategoryData)
+         dispatch(actions.createOrEditSubcategoryItem(editedSubcategoryData))
+      }
+    };
+
+    const categoryorSubcategoryFinder = (type)=>{
+        if(type === "category-item" || type === "edit-category-item"){
+            return modalData.id ? "Edit Category Item" : "Add Category Item"
+        }else{
+            return modalData.id ? "Edit Subcategory Item" : "Add Subcategory Item"
+        }
+    }
+
   return (
      <Dialog open={open} onClose={()=>onClose()} maxWidth="sm" fullWidth>
         <DialogTitle>
-          {additionType === "main-menu"
+          {categoryorSubcategoryFinder(additionType)}
+          {/* {additionType === "category-item" 
             ? modalData.id
-              ? "Edit Menu Item"
-              : "Add Menu Item"
+              ? "Edit Category Item"
+              : "Add Category Item"
             : modalData.id
-            ? "Edit Submenu Item"
-            : "Add Submenu Item"}
+            ? "Edit Subcategory Item"
+            : "Add Subcategory Item"} */}
         </DialogTitle>
         <DialogContent sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
           <TextField
@@ -38,6 +116,14 @@ export default function AssetMenuBuilderDialogue({
             value={modalData.title || ""}
             onChange={(e) =>
               setModalData({ ...modalData, title: e.target.value })
+            }
+          />
+           <TextField
+            label="Code"
+            fullWidth
+            value={modalData.code || ""}
+            onChange={(e) =>
+              setModalData({ ...modalData, code: e.target.value })
             }
           />
           <TextField
@@ -53,11 +139,8 @@ export default function AssetMenuBuilderDialogue({
           <Button onClick={onClose}>Cancel</Button>
           <Button
             onClick={()=>{
-                if(additionType && additionType == "main-menu"){
-                    handleChildData("main-menu")
-                }else{
-                    handleChildData("sub-menu")
-                }
+              handleCreateCategoryOrSubcategory()
+              onClose()
             }}
             variant="contained"
             sx={{ bgcolor: "#f46b45", "&:hover": { bgcolor: "#d93d04" } }}
--- src/stores/actions/index.js
+++ src/stores/actions/index.js
@@ -2,3 +2,4 @@ export * from './users.actions';
 export * from './login.actions';
 export * from './menubuilder.actions';
 export * from './ticket.action';
+export * from './asset.actions';
