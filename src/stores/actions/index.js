b14e637a3e99fecf24475bb485c5a23b476ac0cd
Komal Singh <komalcaerusit@gmail.com>
2025-10-28 12:14:04 +0530

    assets api integration

--- src/stores/actions/index.js
+++ src/stores/actions/index.js
@@ -2,4 +2,5 @@ export * from './users.actions';
 export * from './login.actions';
 export * from './menubuilder.actions';
 export * from './ticket.action';
+export * from './categorySubcategory.actions';
 export * from './asset.actions';
